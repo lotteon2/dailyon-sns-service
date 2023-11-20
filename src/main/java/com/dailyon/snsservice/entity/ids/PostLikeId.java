cac318fa951a5a2f3c7393592b3a7bed58dfd37a
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-20 11:58:48 +0900

    [REFACTOR] 복합키 id 클래스 수정 및 클래스 이름 변경

--- src/main/java/com/dailyon/snsservice/entity/ids/PostLikeId.java
+++ src/main/java/com/dailyon/snsservice/entity/ids/PostLikeId.java
@@ -1,16 +1,13 @@
 package com.dailyon.snsservice.entity.ids;
 
-import com.dailyon.snsservice.entity.Member;
-import com.dailyon.snsservice.entity.Post;
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
-
-import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
 public class PostLikeId implements Serializable {
 
-  private Member member;
-  private Post post;
+  private Long member;
+  private Long post;
 }
