1c8ca438fb675c30fb1b2ab32451fa35ffc8ab74
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-20 15:47:25 +0900

    O2F-146 [FEAT] 댓글 등록 Controller 로직 및 테스트코드 추가

---
+++ src/test/java/com/dailyon/snsservice/controller/rest/CommentApiControllerTest.java
@@ -0,0 +1,49 @@
+package com.dailyon.snsservice.controller.rest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+import com.dailyon.snsservice.dto.request.post.CreateCommentRequest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+@AutoConfigureMockMvc
+@ActiveProfiles(value = {"test"})
+class CommentApiControllerTest {
+
+  @Autowired private MockMvc mockMvc;
+
+  private final ObjectMapper objectMapper = new ObjectMapper();
+
+  @Test
+  @DisplayName("댓글 등록")
+  void createComment() throws Exception {
+    // given
+    Long memberId = 1L;
+    Long postId = 1L;
+    CreateCommentRequest createCommentRequest =
+        CreateCommentRequest.builder().description("댓글 123").build();
+
+    String requestBody = objectMapper.writeValueAsString(createCommentRequest);
+
+    // when, then
+    mockMvc
+        .perform(
+            post("/posts/{postId}/comments", postId)
+                .header("memberId", memberId)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(requestBody))
+        .andExpect(MockMvcResultMatchers.status().isCreated());
+  }
+}
