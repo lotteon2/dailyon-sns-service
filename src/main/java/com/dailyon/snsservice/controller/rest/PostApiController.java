1abcdeaed55c16863f74cad4294dde8a14fa669a
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-20 14:27:38 +0900

    O2F-318 [FEAT] 게시글 좋아요 목록 조회 Controller 및 테스트 로직 작성

--- src/main/java/com/dailyon/snsservice/controller/rest/PostApiController.java
+++ src/main/java/com/dailyon/snsservice/controller/rest/PostApiController.java
@@ -3,6 +3,7 @@ package com.dailyon.snsservice.controller.rest;
 import com.dailyon.snsservice.dto.request.post.CreatePostRequest;
 import com.dailyon.snsservice.dto.request.post.UpdatePostRequest;
 import com.dailyon.snsservice.dto.response.post.CreatePostResponse;
+import com.dailyon.snsservice.dto.response.post.PostLikePageResponse;
 import com.dailyon.snsservice.dto.response.post.PostPageResponse;
 import com.dailyon.snsservice.dto.response.post.UpdatePostResponse;
 import com.dailyon.snsservice.service.PostService;
@@ -12,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/posts")
@@ -21,16 +23,16 @@ public class PostApiController {
 
   private final PostService postService;
 
-  @ResponseStatus(HttpStatus.CREATED)
   @PostMapping
-  public CreatePostResponse createPost(
+  public ResponseEntity<CreatePostResponse> createPost(
       @RequestHeader(name = "memberId") Long memberId,
       @Valid @RequestBody CreatePostRequest createPostRequest) {
-    return postService.createPost(memberId, createPostRequest);
+    CreatePostResponse createPostResponse = postService.createPost(memberId, createPostRequest);
+    return ResponseEntity.status(HttpStatus.CREATED).body(createPostResponse);
   }
 
   @GetMapping
-  public PostPageResponse getPosts(
+  public ResponseEntity<PostPageResponse> getPosts(
       @RequestHeader(name = "memberId", required = false) Long memberId,
       @PageableDefault(
               page = 0,
@@ -38,21 +40,36 @@ public class PostApiController {
               sort = {"viewCount"},
               direction = Sort.Direction.DESC)
           Pageable pageable) {
-    return postService.getPosts(memberId, pageable);
+    PostPageResponse postPageResponse = postService.getPosts(memberId, pageable);
+    return ResponseEntity.ok(postPageResponse);
   }
 
   @PutMapping("/{postId}")
-  public UpdatePostResponse updatePost(
-          @RequestHeader(name = "memberId") Long memberId,
-          @PathVariable("postId") Long postId,
-          @Valid @RequestBody UpdatePostRequest updatePostRequest) {
-    return postService.updatePost(postId, updatePostRequest);
+  public ResponseEntity<UpdatePostResponse> updatePost(
+      @RequestHeader(name = "memberId") Long memberId,
+      @PathVariable("postId") Long postId,
+      @Valid @RequestBody UpdatePostRequest updatePostRequest) {
+    UpdatePostResponse updatePostResponse = postService.updatePost(postId, updatePostRequest);
+    return ResponseEntity.ok(updatePostResponse);
   }
 
   @DeleteMapping("/{postId}")
-  public void deletePost(
-      @RequestHeader(name = "memberId", required = false) Long memberId,
-      @PathVariable("postId") Long postId) {
+  public ResponseEntity<Void> deletePost(
+      @RequestHeader(name = "memberId") Long memberId, @PathVariable("postId") Long postId) {
     postService.softDeletePost(postId);
+    return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/likes")
+  public ResponseEntity<PostLikePageResponse> getPostLikes(
+      @RequestHeader(name = "memberId") Long memberId,
+      @PageableDefault(
+              page = 0,
+              size = 8,
+              sort = {"viewCount"},
+              direction = Sort.Direction.DESC)
+          Pageable pageable) {
+    PostLikePageResponse postLikePageResponse = postService.getPostLikes(memberId, pageable);
+    return ResponseEntity.ok(postLikePageResponse);
   }
 }
