c0f0747fa7a7dc010e7fb0893def9c4dcf73240a
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-21 10:28:31 +0900

    O2F-342 [FEAT] 팔로워 목록 조회 Dao 로직 및 테스트코드 작성

--- src/main/java/com/dailyon/snsservice/repository/member/MemberJpaRepository.java
+++ src/main/java/com/dailyon/snsservice/repository/member/MemberJpaRepository.java
@@ -1,5 +1,6 @@
 package com.dailyon.snsservice.repository.member;
 
+import com.dailyon.snsservice.dto.response.follow.FollowerResponse;
 import com.dailyon.snsservice.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -15,4 +16,14 @@ public interface MemberJpaRepository extends JpaRepository<Member, Long> {
               + "where f.follower.id = :followerId",
       countQuery = "select count(m) from Member m")
   Page<Member> findFollowingsByFollowerId(Long followerId, Pageable pageable);
+
+  @Query(
+      value =
+          "select new com.dailyon.snsservice.dto.response.follow.FollowerResponse(m.id, m.nickname, m.profileImgUrl, "
+              + "exists (select m2 from Member m2 left join m2.followers f2 where f2.following.id = m.id and f2.follower.id = :followingId)) "
+              + "from Member m "
+              + "left join m.followers f "
+              + "where f.following.id = :followingId",
+      countQuery = "select count(m) from Member m")
+  Page<FollowerResponse> findFollowersByFollowingId(Long followingId, Pageable pageable);
 }
--- src/main/java/com/dailyon/snsservice/repository/member/MemberRepositoryImpl.java
+++ src/main/java/com/dailyon/snsservice/repository/member/MemberRepositoryImpl.java
@@ -1,5 +1,6 @@
 package com.dailyon.snsservice.repository.member;
 
+import com.dailyon.snsservice.dto.response.follow.FollowerResponse;
 import com.dailyon.snsservice.entity.Member;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -13,7 +14,12 @@ public class MemberRepositoryImpl implements MemberRepository {
   private final MemberJpaRepository memberJpaRepository;
 
   @Override
-  public Page<Member> findFollowingsByFollowerId(Long followingId, Pageable pageable) {
-    return memberJpaRepository.findFollowingsByFollowerId(followingId, pageable);
+  public Page<Member> findFollowingsByFollowerId(Long followerId, Pageable pageable) {
+    return memberJpaRepository.findFollowingsByFollowerId(followerId, pageable);
+  }
+
+  @Override
+  public Page<FollowerResponse> findFollowersByFollowingId(Long followingId, Pageable pageable) {
+    return memberJpaRepository.findFollowersByFollowingId(followingId, pageable);
   }
 }
