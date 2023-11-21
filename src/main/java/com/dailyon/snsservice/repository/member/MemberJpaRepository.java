254e913078310fbeb1ee771456e63e52470010b2
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-21 11:40:16 +0900

    O2F-361 [FEAT] OOTD 사용자 프로필 조회 Dao 로직 및 테스트코드 작성

--- src/main/java/com/dailyon/snsservice/repository/member/MemberJpaRepository.java
+++ src/main/java/com/dailyon/snsservice/repository/member/MemberJpaRepository.java
@@ -1,11 +1,14 @@
 package com.dailyon.snsservice.repository.member;
 
 import com.dailyon.snsservice.dto.response.follow.FollowerResponse;
+import com.dailyon.snsservice.dto.response.member.OOTDMemberProfileResponse;
 import com.dailyon.snsservice.entity.Member;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface MemberJpaRepository extends JpaRepository<Member, Long> {
 
@@ -26,4 +29,10 @@ public interface MemberJpaRepository extends JpaRepository<Member, Long> {
               + "where f.following.id = :followingId",
       countQuery = "select count(m) from Member m")
   Page<FollowerResponse> findFollowersByFollowingId(Long followingId, Pageable pageable);
+
+  @Query(
+      "select new com.dailyon.snsservice.dto.response.member.OOTDMemberProfileResponse(m.id, m.nickname, m.profileImgUrl, m.followingCount, m.followerCount, "
+          + "exists (select f from Follow f where f.follower.id = :followerId and f.following.id = :memberId)) "
+          + "from Member m where m.id = :memberId")
+  Optional<OOTDMemberProfileResponse> findOOTDMemberProfile(Long memberId, Long followerId);
 }
--- src/main/java/com/dailyon/snsservice/repository/member/MemberRepositoryImpl.java
+++ src/main/java/com/dailyon/snsservice/repository/member/MemberRepositoryImpl.java
@@ -1,11 +1,15 @@
 package com.dailyon.snsservice.repository.member;
 
 import com.dailyon.snsservice.dto.response.follow.FollowerResponse;
+import com.dailyon.snsservice.dto.response.member.OOTDMemberProfileResponse;
 import com.dailyon.snsservice.entity.Member;
+import com.dailyon.snsservice.exception.MemberEntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -21,5 +25,12 @@ public class MemberRepositoryImpl implements MemberRepository {
   @Override
   public Page<FollowerResponse> findFollowersByFollowingId(Long followingId, Pageable pageable) {
     return memberJpaRepository.findFollowersByFollowingId(followingId, pageable);
+  }
+
+  @Override
+  public OOTDMemberProfileResponse findOOTDMemberProfile(Long memberId, Long followerId) {
+    return memberJpaRepository
+        .findOOTDMemberProfile(memberId, followerId)
+        .orElseThrow(MemberEntityNotFoundException::new);
   }
 }
