69415d53d96ffca067ec08a70818d42407e4bab1
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-24 12:18:08 +0900

    [REFACTOR] 팔로잉 목록 조회 기준테이블 변경
    - 회원 -> 팔로우로 기준 테이블 변경

--- src/main/java/com/dailyon/snsservice/repository/member/MemberJpaRepository.java
+++ src/main/java/com/dailyon/snsservice/repository/member/MemberJpaRepository.java
@@ -14,14 +14,6 @@ public interface MemberJpaRepository extends JpaRepository<Member, Long> {
 
   @Query(
       value =
-          "select m from Member m "
-              + "join fetch m.following f "
-              + "where f.follower.id = :followerId",
-      countQuery = "select count(m) from Member m")
-  Page<Member> findFollowingsByFollowerId(Long followerId, Pageable pageable);
-
-  @Query(
-      value =
           "select new com.dailyon.snsservice.dto.response.follow.FollowerResponse(m.id, m.nickname, m.profileImgUrl, "
               + "exists (select m2 from Member m2 left join m2.followers f2 where f2.following.id = m.id and f2.follower.id = :followingId)) "
               + "from Member m "
