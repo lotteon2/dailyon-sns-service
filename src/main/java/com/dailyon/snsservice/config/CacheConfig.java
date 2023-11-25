de45cac928e9d309e932066375909443d75a46ac
CokeLee777 <akak640@kyonggi.ac.kr>
2023-11-25 16:01:06 +0900

    [FEAT] Top4 OOTD 캐시 추가
    - 캐시 읽기 Look Aside 전략으로 추가

--- src/main/java/com/dailyon/snsservice/config/CacheConfig.java
+++ src/main/java/com/dailyon/snsservice/config/CacheConfig.java
@@ -1,12 +1,14 @@
 package com.dailyon.snsservice.config;
 
 import com.dailyon.snsservice.vo.PostCountVO;
+import com.dailyon.snsservice.vo.Top4OOTDVO;
+import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.time.Duration;
-
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import java.time.Duration;
+import java.util.List;
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -44,21 +46,40 @@ public class CacheConfig {
   @Bean
   public RedisCacheManagerBuilderCustomizer redisCacheManagerBuilderCustomizer(
       ObjectMapper objectMapper) {
-    Jackson2JsonRedisSerializer<PostCountVO> jackson2JsonRedisSerializer =
+    Jackson2JsonRedisSerializer<PostCountVO> postCountVOJackson2JsonRedisSerializer =
         new Jackson2JsonRedisSerializer<>(PostCountVO.class);
-    jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+
+    JavaType top4OOTDVOListType =
+        objectMapper.getTypeFactory().constructCollectionType(List.class, Top4OOTDVO.class);
+    Jackson2JsonRedisSerializer<List<Top4OOTDVO>> top4OOTDVOJackson2JsonRedisSerializer =
+        new Jackson2JsonRedisSerializer<>(top4OOTDVOListType);
+
+    postCountVOJackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+    top4OOTDVOJackson2JsonRedisSerializer.setObjectMapper(objectMapper);
 
     return (builder ->
-        builder.withCacheConfiguration(
-            "postCount",
-            RedisCacheConfiguration.defaultCacheConfig()
-                .entryTtl(Duration.ofDays(1))
-                .disableCachingNullValues()
-                .serializeKeysWith(
-                    RedisSerializationContext.SerializationPair.fromSerializer(
-                        new StringRedisSerializer()))
-                .serializeValuesWith(
-                    RedisSerializationContext.SerializationPair.fromSerializer(
-                        jackson2JsonRedisSerializer))));
+        builder
+            .withCacheConfiguration(
+                "postCount",
+                RedisCacheConfiguration.defaultCacheConfig()
+                    .entryTtl(Duration.ofDays(1))
+                    .disableCachingNullValues()
+                    .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                            new StringRedisSerializer()))
+                    .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                            postCountVOJackson2JsonRedisSerializer)))
+            .withCacheConfiguration(
+                "top4OOTD",
+                RedisCacheConfiguration.defaultCacheConfig()
+                    .entryTtl(Duration.ofDays(1))
+                    .disableCachingNullValues()
+                    .serializeKeysWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                            new StringRedisSerializer()))
+                    .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(
+                            top4OOTDVOJackson2JsonRedisSerializer))));
   }
 }
