75c01fd3b68aba6d8c0f05b3db03da232548c16b
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-08 12:03:46 +0900

    [FIX] Feign Exception 부모 class 변경

--- src/main/java/com/dailyon/snsservice/exception/feign/ProductServiceOutOfServiceException.java
+++ src/main/java/com/dailyon/snsservice/exception/feign/ProductServiceOutOfServiceException.java
@@ -1,18 +1,10 @@
 package com.dailyon.snsservice.exception.feign;
 
-import com.dailyon.snsservice.exception.common.CustomException;
-import org.springframework.http.HttpStatus;
-
-public class ProductServiceOutOfServiceException extends CustomException {
+public class ProductServiceOutOfServiceException extends RuntimeException {
 
   private static final String message = "상품 서비스에 문제가 있습니다.";
 
   public ProductServiceOutOfServiceException() {
     super(message);
-  }
-
-  @Override
-  public HttpStatus getStatusCode() {
-    return HttpStatus.INTERNAL_SERVER_ERROR;
   }
 }
--- src/main/java/com/dailyon/snsservice/exception/feign/PromotionServiceOutOfServiceException.java
+++ src/main/java/com/dailyon/snsservice/exception/feign/PromotionServiceOutOfServiceException.java
@@ -1,18 +1,10 @@
 package com.dailyon.snsservice.exception.feign;
 
-import com.dailyon.snsservice.exception.common.CustomException;
-import org.springframework.http.HttpStatus;
-
-public class PromotionServiceOutOfServiceException extends CustomException {
+public class PromotionServiceOutOfServiceException extends RuntimeException {
 
   private static final String message = "프로모션 서비스에 문제가 있습니다.";
 
   public PromotionServiceOutOfServiceException() {
     super(message);
-  }
-
-  @Override
-  public HttpStatus getStatusCode() {
-    return HttpStatus.INTERNAL_SERVER_ERROR;
   }
 }
