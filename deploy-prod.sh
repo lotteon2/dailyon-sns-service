2f23f4d6a57dd3f9d28546f5ed2dd552bc732cdc
CokeLee777 <akak640@kyonggi.ac.kr>
2023-12-28 16:54:40 +0900

    [FIX] fix shell script

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -15,12 +15,14 @@ aws eks update-kubeconfig --region ap-northeast-2 --name $AWS_EKS_CLUSTER_NAME
 
 if kubectl get pv "${pv_name}" -n "${namespace}" &> /dev/null; then
   echo "PersistenceVolume is already exists"
+  kubectl apply -f ./pv-prod.yml
 else
   kubectl create -f ./pv-prod.yml
 fi
 
 if kubectl get pvc "${pvc_name}" -n "${namespace}" &> /dev/null; then
   echo "PersistenceVolumeClaim is already exists"
+  kubectl apply -f ./pvc-prod.yml
 else
   kubectl create -f ./pvc-prod.yml
 fi
