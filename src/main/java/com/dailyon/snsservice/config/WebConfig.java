d32c20434b23db136a87f47129f90ec4484421df
CokeLee777 <akak640@kyonggi.ac.kr>
2024-01-02 19:41:54 +0900

    [TEST] revise xray logic

--- src/main/java/com/dailyon/snsservice/config/WebConfig.java
+++ src/main/java/com/dailyon/snsservice/config/WebConfig.java
@@ -1,28 +1,14 @@
 package com.dailyon.snsservice.config;
 
-import com.amazonaws.xray.AWSXRay;
-import com.amazonaws.xray.AWSXRayRecorderBuilder;
 import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
-import com.amazonaws.xray.plugins.EC2Plugin;
-import com.amazonaws.xray.strategy.sampling.CentralizedSamplingStrategy;
+import javax.servlet.Filter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import javax.servlet.Filter;
-import java.net.URL;
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
-  static {
-    AWSXRayRecorderBuilder builder = AWSXRayRecorderBuilder.standard().withPlugin(new EC2Plugin());
-
-    URL ruleFile = WebConfig.class.getResource("/sampling-rules.json");
-    builder.withSamplingStrategy(new CentralizedSamplingStrategy(ruleFile));
-    AWSXRay.setGlobalRecorder(builder.build());
-  }
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
