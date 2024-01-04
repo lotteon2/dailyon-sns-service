c10cc2921894c5d2fc518183ab43eecdcc44e516
CokeLee777 <akak640@kyonggi.ac.kr>
2024-01-04 12:31:24 +0900

    [CHORE] scale up resource limit and revise terminationGracePeriodSeconds and remove xray spec

--- src/main/java/com/dailyon/snsservice/SnsServiceApplication.java
+++ src/main/java/com/dailyon/snsservice/SnsServiceApplication.java
@@ -1,18 +1,14 @@
 package com.dailyon.snsservice;
 
+import java.util.TimeZone;
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import javax.annotation.PostConstruct;
-import java.time.LocalDateTime;
-import java.util.TimeZone;
-
-@EnableAspectJAutoProxy
 @EnableScheduling
 @EnableFeignClients
 @EnableJpaAuditing
--- src/main/java/com/dailyon/snsservice/config/WebConfig.java
+++ src/main/java/com/dailyon/snsservice/config/WebConfig.java
@@ -1,8 +1,5 @@
 package com.dailyon.snsservice.config;
 
-import com.amazonaws.xray.javax.servlet.AWSXRayServletFilter;
-import javax.servlet.Filter;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,10 +10,5 @@ public class WebConfig implements WebMvcConfigurer {
   @Override
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**").allowedOrigins("*").allowedHeaders("*").allowedMethods("*");
-  }
-
-  @Bean
-  public Filter tracingFilter() {
-    return new AWSXRayServletFilter("Scorekeep");
   }
 }
